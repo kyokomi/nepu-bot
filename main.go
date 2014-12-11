a5bd11a9b274da38e6e3759f3ad2ceb67e9a52b5
kyokomi <kyoko1220adword@gmail.com>
2014-12-12 08:52:59 +0900

    init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/brettbuddin/victor"
+)
+
+func main() {
+	bot := victor.New(victor.Config{
+		Name:         "slackbot",
+		ChatAdapter:  "slack",
+		StoreAdapter: "memory",
+		HTTPAddr:     ":8000",
+	})
+
+	bot.HandleCommandFunc("hello|hi|howdy", func(s victor.State) {
+		s.Chat().Send(s.Message().ChannelID(), fmt.Sprintf("Hello, %s", s.Message().UserName()))
+	})
+
+	go bot.Run()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	<-sigs
+
+	bot.Stop()
+}
