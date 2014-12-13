e773da86626432cdbc975da5b23b776ca675a623
kyokomi <kyoko1220adword@gmail.com>
2014-12-13 23:27:46 +0900

    いーすんにしてdocomoの雑談APIを使うようにした

--- main.go
+++ main.go
@@ -5,19 +5,36 @@ import (
 	"os"
 	"os/signal"
 
+	"encoding/json"
+	"log"
+
 	"github.com/brettbuddin/victor"
+	"github.com/kyokomi/gobot/docomogo"
 )
 
 func main() {
 	bot := victor.New(victor.Config{
-		Name:         "slackbot",
+		Name:         "いーすん",
 		ChatAdapter:  "slack",
 		StoreAdapter: "memory",
 		HTTPAddr:     ":8000",
 	})
 
+	d := docomogo.NewDialogue()
+
 	bot.HandleCommandFunc("hello|hi|howdy", func(s victor.State) {
 		s.Chat().Send(s.Message().ChannelID(), fmt.Sprintf("Hello, %s", s.Message().UserName()))
+	})
+	bot.HandleCommandFunc(".*", func(s victor.State) {
+
+		res := d.Send(s.Message().Text())
+
+		var resMap map[string]string
+		if err := json.Unmarshal(res, &resMap); err != nil {
+			log.Fatalln("Unmarshal ", err)
+		}
+
+		s.Chat().Send(s.Message().ChannelID(), resMap["utt"])
 	})
 
 	go bot.Run()
