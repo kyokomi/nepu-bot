5f6f4c8f4c6f73f99ae267ee1749c6f38a8b00a1
kyokomi <kyoko1220adword@gmail.com>
2014-12-18 14:23:12 +0900

    godep save

--- Godeps/_workspace/src/github.com/kyokomi/go-docomo/zatudan.go
+++ Godeps/_workspace/src/github.com/kyokomi/go-docomo/zatudan.go
@@ -1,11 +1,11 @@
 package docomo
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"bytes"
 )
 
 func (d *DocomoClient) SendZatsudan(nickname, message string) ([]byte, error) {
@@ -13,25 +13,25 @@ func (d *DocomoClient) SendZatsudan(nickname, message string) ([]byte, error) {
 	// Mode dialog or srtr
 	// CharactorID なし:デフォルト 20:関西弁 30:あかちゃん
 	type DialogueBody struct {
-		Utt      string `json:"utt"`
-		Context  string `json:"context"`
-		Nickname string `json:"nickname"`
-		//		NicknameYomi   string `json:"nickname_y"`
-		//		Sex            string `json:"sex"`
-		//		Bloodtype      string `json:"bloodtype"`
-		//		BirthdateY     int    `json:"birthdateY"`
-		//		BirthdateM     int    `json:"birthdateM"`
-		//		BirthdateD     int    `json:"birthdateD"`
-		//		Age            int    `json:"age"`
-		//		Constellations string `json:"constellations"`
-		//		Place          string `json:"place"`
-		//		Mode           string `json:"mode"`
-		//		CharactorID    int    `json:"t"`
+		Utt            string `json:"utt"`
+		Context        string `json:"context"`
+		Nickname       string `json:"nickname"`
+//		NicknameYomi   string `json:"nickname_y"`
+//		Sex            string `json:"sex"`
+//		Bloodtype      string `json:"bloodtype"`
+//		BirthdateY     int    `json:"birthdateY"`
+//		BirthdateM     int    `json:"birthdateM"`
+//		BirthdateD     int    `json:"birthdateD"`
+//		Age            int    `json:"age"`
+//		Constellations string `json:"constellations"`
+//		Place          string `json:"place"`
+//		Mode           string `json:"mode"`
+//		CharactorID    int    `json:"t"`
 	}
 
 	b := DialogueBody{
-		Utt:      message,
-		Context:  d.context,
+		Utt:     message,
+		Context: d.context,
 		Nickname: nickname,
 	}
 
@@ -41,7 +41,7 @@ func (d *DocomoClient) SendZatsudan(nickname, message string) ([]byte, error) {
 		return nil, err
 	}
 
-	res, err := d.PostJSON(DIALOGUE_URL, bytes.NewReader(data))
+	res, err := d.post(DIALOGUE_URL, "application/json", bytes.NewReader(data))
 	if err != nil {
 		return nil, err
 	}
@@ -57,7 +57,6 @@ func (d *DocomoClient) SendZatsudan(nickname, message string) ([]byte, error) {
 			return nil, err
 		}
 		d.context = resMap["context"]
-		fmt.Println("context = ", d.context)
 	} else {
 		fmt.Println(string(resData))
 	}
