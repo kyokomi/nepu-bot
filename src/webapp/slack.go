1843b74cc966ce61d88e422b466e73d69f11556b
kyokomi <kyoko1220adword@gmail.com>
2015-01-07 20:08:04 +0900

    fix #7 教えてモードで回答なしで固定文言を返すように

--- src/webapp/slack.go
+++ src/webapp/slack.go
@@ -17,6 +17,7 @@ import (
 var logger = log.New(os.Stdout, "nepu-bot", log.Llongfile)
 var random = rand.New(rand.NewSource(1))
 
+// Kaomoji 顔文字
 var Kaomoji = []string{
 	"(; ・∀・)",
 	"(~_~;)",
@@ -27,21 +28,25 @@ var Kaomoji = []string{
 	"m9( ﾟдﾟ)",
 }
 
+// SlackClient is Slack IncomingURL Client.
 type SlackClient struct {
 	Name             string
 	SlackIncomingURL string
 }
 
+// Message is Slack Receive Message.
 type Message struct {
 	userID, userName, channelID, channelName, text string
 }
 
+// OutgoingMessage is Slack PostRequest Message.
 type OutgoingMessage struct {
 	Channel  string `json:"channel"`
 	Username string `json:"username"`
 	Text     string `json:"text"`
 }
 
+// HubotSlackWebhook hubotとしてSlackのWebhockを待ち受けるHandleFunc
 func HubotSlackWebhook(c web.C, _ http.ResponseWriter, r *http.Request) {
 
 	slackClient := c.Env["slack"].(*SlackClient)
@@ -66,46 +71,48 @@ func HubotSlackWebhook(c web.C, _ http.ResponseWriter, r *http.Request) {
 	}
 
 	var resMessage string
-	if containsArray(m.text, []string{"おしえて", "教えて"}) {
+	switch {
+	default:
+		// その他は全部雑談
+
+		// 雑談API呼び出し
+		dq := DialogueRequest{}
+		dq.Nickname = m.userName
+		res, err := docomoClient.SendZatsudan(dq, true)
+		if err != nil {
+			logger.Println(err)
+			return
+		}
+		resMessage = res.Utt
+
+	case containsArray(m.text, []string{"おしえて", "教えて"}):
 		// 知識Q&A
 		qa := docomo.QARequest{}
 		qa.QAText = m.text
 		for _, word := range []string{"おしえて", "教えて"} {
 			qa.QAText = strings.Replace(qa.QAText, word, "", -1)
 		}
-		res, err := docomoClient.SendQA(&qa)
-		if err != nil {
-			logger.Println(err)
-			return
-		}
-		resMessage = res.Answers[0].AnswerText
-
-	} else {
-		// その他は全部雑談
-
-		// 雑談API呼び出し
-		res, err := docomoClient.SendZatsudan(m.userName, t)
+		res, err := docomoClient.SendQA(qa)
 		if err != nil {
 			logger.Println(err)
 			return
 		}
 
-		var resMap map[string]string
-		if err := json.Unmarshal(res, &resMap); err != nil {
-			logger.Println("Unmarshal ", err)
-			return
+		if res.Success() {
+			resMessage = res.Answers[0].AnswerText
+		} else {
+			resMessage = "はて?"
 		}
-		resMessage = resMap["utt"]
 	}
 
 	// 顔文字をランダムで付与する
 	idx := random.Int31n((int32)(len(Kaomoji) - 1))
 	message := resMessage + " " + Kaomoji[idx]
 	// 結果を非同期でSlackへ
-	go slackClient.Send(m.channelID, message)
+	go slackClient.send(m.channelID, message)
 }
 
-func (s SlackClient) Send(channelID, msg string) {
+func (s SlackClient) send(channelID, msg string) {
 	body, err := json.Marshal(&OutgoingMessage{
 		Channel:  channelID,
 		Username: s.Name,
@@ -119,8 +126,6 @@ func (s SlackClient) Send(channelID, msg string) {
 		logger.Println("error sending to chat:", err)
 	}
 }
-
-//
 
 func containsArray(s string, substrs []string) bool {
 	for _, substr := range substrs {
